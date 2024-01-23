b56fde1bfc0a6acac0cc97b2d90b391c6ecd83ca
adii1203 <aditya32ft@gmail.com>
2024-01-23 21:22:19 +0530

    password logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [password, setPassword] = useState("password");
+  const [passwordlength, setPasswordLength] = useState(12);
+  const [options, setOptions] = useState({
+    number: true,
+    upperCase: true,
+    lowerCase: true,
+    specialCharacter: true,
+  });
   return (
     <>
+      <h1>hello codedamn</h1>
+      <input
+        value={password}
+        type="text"
+        style={{
+          color: "white",
+          border: "none",
+          backgroundColor: "transparent",
+          outline: "none",
+          fontSize: "20px",
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          marginTop: "20px",
+        }}>
+        <button>Generate Password</button>
+        <button>Copy</button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+          marginTop: "20px",
+        }}>
+        <label>Length</label>
+        <input
+          value={passwordlength}
+          type="range"
+          name="range"
+          id="range"
+          min={10}
+          max={25}
+          onChange={(e) => setPasswordLength(e.target.value)}
+        />
+      </div>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          checked={options.number}
+          onChange={(e) => setOptions({ ...options, number: e.target.checked })}
+          type="checkbox"
+          name="number"
+          id="number"
+        />
+        <label htmlFor="number">Number</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <input
+          checked={options.upperCase}
+          onChange={(e) =>
+            setOptions({ ...options, upperCase: e.target.checked })
+          }
+          type="checkbox"
+          name="upperCase"
+          id="upperCase"
+        />
+        <label htmlFor="upperCase">Upper Case</label>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <input
+          checked={options.lowerCase}
+          onChange={(e) =>
+            setOptions({ ...options, lowerCase: e.target.checked })
+          }
+          type="checkbox"
+          name="lowerCase"
+          id="lowerCase"
+        />
+        <label htmlFor="lowerCase">Lower Case</label>
+      </div>
+      <div>
+        <input
+          checked={options.specialCharacter}
+          onChange={(e) =>
+            setOptions({ ...options, specialCharacter: e.target.checked })
+          }
+          type="checkbox"
+          name="specialCharacter"
+          id="specialCharacter"
+        />
+        <label htmlFor="specialCharacter">Special Character</label>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
